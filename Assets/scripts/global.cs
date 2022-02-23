a2e7a3fab98535c7c539e8674b595d4b49f71a09
Alfred0801 <blockchain4webdev@gmail.com>
2022-02-24 01:12:47 +0900

    first commit

---
+++ Assets/scripts/global.cs
@@ -0,0 +1,14 @@
+public class APIForm
+{
+    public string msg;
+    public float total;
+    public int[] cardOder;
+    public string serverMsg;
+    public bool[] activeArray;
+    public string result;
+}
+
+public class Globalinitial
+{
+    public string BaseUrl = "http://31.220.58.193:443/";
+}
